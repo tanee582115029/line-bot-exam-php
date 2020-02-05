8419c05f4adc6f5c901f9c8542374fe631799d36
Tanee Yooyen <tiwly23060@gmail.com>
2020-02-05 10:45:39 +0700

    update push secret

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = '0pl2UeOgbDBXo4VBQtmcK7cV12ftdHE/Exal56JNSpwPWYpVq7mbDOqmLlBKsj9z9ou/9wy44JbrvYW36om0tdHdyb4vfTdz+bDjE8av6Az1HuAj14V5NEq4FoO9JoJafHR3lXL7joEn16Up+SgclwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '40759e175abb9cc0452b12c16f52e0ea';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
