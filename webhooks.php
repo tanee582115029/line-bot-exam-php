09c287f6fe1698a3fca46b683d2fc7f7c3fdff0b
Tanee Yooyen <tiwly23060@gmail.com>
2020-02-05 12:05:42 +0700

    ok

--- webhooks.php
+++ webhooks.php
@@ -124,183 +124,122 @@ $jsonSlipFlex = [
 ];
 
 
-$jsonPromoFlex = '{
-	"type": "carousel",
-	"contents": [
-	  {
-		"type": "bubble",
-		"hero": {
-		  "type": "image",
-		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover",
-		  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-		},
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "text",
-			  "text": "Arm Chair, White",
-			  "wrap": true,
-			  "weight": "bold",
-			  "size": "xl"
-			},
-			{
-			  "type": "box",
-			  "layout": "baseline",
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "$49",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "xl",
-				  "flex": 0
-				},
-				{
-				  "type": "text",
-				  "text": ".99",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "sm",
-				  "flex": 0
-				}
+$jsonPromoFlex = [
+	"type" => "flex",
+	"altText" => "Hello Flex Message",
+	"contents" => [
+	  "type" => "bubble",
+	  "direction" => "ltr",
+	  "header" => [
+		"type" => "box",
+		"layout" => "vertical",
+		"contents" => [
+		  [
+			"type" => "text",
+			"text" => "Promotion",
+			"size" => "lg",
+			"align" => "start",
+			"weight" => "bold",
+			"color" => "#009813"
+		  ],
+		  [
+			"type" => "text",
+			"text" => "50 %",
+			"size" => "3xl",
+			"weight" => "bold",
+			"color" => "#000000"
+		  ],
+		  [
+			"type" => "text",
+			"text" => "Rabbit Line Pay",
+			"size" => "lg",
+			"weight" => "bold",
+			"color" => "#000000"
+		  ],
+		  [
+			"type" => "text",
+			"text" => "2019.02.14 21:47 (GMT+0700)",
+			"size" => "xs",
+			"color" => "#B2B2B2"
+		  ],
+		  [
+			"type" => "text",
+			"text" => "Payment complete.",
+			"margin" => "lg",
+			"size" => "lg",
+			"color" => "#000000"
+		  ]
+		]
+	  ],
+	  "body" => [
+		"type" => "box",
+		"layout" => "vertical",
+		"contents" => [
+		  [
+			"type" => "separator",
+			"color" => "#C3C3C3"
+		  ],
+		  [
+			"type" => "box",
+			"layout" => "baseline",
+			"margin" => "lg",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "Merchant",
+				"align" => "start",
+				"color" => "#C3C3C3"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "BTS 01",
+				"align" => "end",
+				"color" => "#000000"
 			  ]
-			}
-		  ]
-		},
-		"footer": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "style": "primary",
-			  "action": {
-				"type": "uri",
-				"label": "Add to Cart",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "button",
-			  "action": {
-				"type": "uri",
-				"label": "Add to wishlist",
-				"uri": "https://linecorp.com"
-			  }
-			}
-		  ]
-		}
-	  },
-	  {
-		"type": "bubble",
-		"hero": {
-		  "type": "image",
-		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover",
-		  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
-		},
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "text",
-			  "text": "Metal Desk Lamp",
-			  "wrap": true,
-			  "weight": "bold",
-			  "size": "xl"
-			},
-			{
-			  "type": "box",
-			  "layout": "baseline",
-			  "flex": 1,
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "$11",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "xl",
-				  "flex": 0
-				},
-				{
-				  "type": "text",
-				  "text": ".99",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "sm",
-				  "flex": 0
-				}
+			]
+		  ],
+		  [
+			"type" => "box",
+			"layout" => "baseline",
+			"margin" => "lg",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "New balance",
+				"color" => "#C3C3C3"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "฿ 45.57",
+				"align" => "end"
 			  ]
-			},
-			{
-			  "type": "text",
-			  "text": "Temporarily out of stock",
-			  "wrap": true,
-			  "size": "xxs",
-			  "margin": "md",
-			  "color": "#ff5551",
-			  "flex": 0
-			}
+			]
+		  ],
+		  [
+			"type" => "separator",
+			"margin" => "lg",
+			"color" => "#C3C3C3"
 		  ]
-		},
-		"footer": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "flex": 2,
-			  "style": "primary",
-			  "color": "#aaaaaa",
-			  "action": {
-				"type": "uri",
-				"label": "Add to Cart",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "button",
-			  "action": {
-				"type": "uri",
-				"label": "Add to wish list",
-				"uri": "https://linecorp.com"
-			  }
-			}
+		]
+	  ],
+	  "footer" => [
+		"type" => "bubble",
+		"layout" => "vertical",
+		"spacing" => "sm",
+		"contents" => [
+		  [
+			"type" => "button",
+            "style" => "primary",
+            "action" => [
+              "type" => "uri",
+              "label" => "Add to Cart",
+              "uri" => "https://linecorp.com"
+			]
 		  ]
-		}
-	  },
-	  {
-		"type": "bubble",
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "flex": 1,
-			  "gravity": "center",
-			  "action": {
-				"type": "uri",
-				"label": "See more",
-				"uri": "https://linecorp.com"
-			  }
-			}
-		  ]
-		}
-	  }
+		]
+	  ]
 	]
-}';
+];
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
