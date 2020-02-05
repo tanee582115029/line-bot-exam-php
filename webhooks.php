82a6bd35e96d9860c8f930413eccce7a4903bd48
Tanee Yooyen <tiwly23060@gmail.com>
2020-02-05 14:55:42 +0700

    cancel

--- webhooks.php
+++ webhooks.php
@@ -107,6 +107,132 @@ $jsonSlipFlex = [
 			]
 		  ]
 		]
+		],
+		"footer" => [
+			"type" => "box",
+			"layout" => "horizontal",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "ยกเลิกการจอง",
+				"size" => "lg",
+				"align" => "center",
+				"color" => "#0084B6",
+				"action" => [
+				  "type" => "uri",
+				  "label" => "ยกเลิกการจอง",
+				  "uri" => "https://google.co.th/"
+				]
+			  ]
+			]
+		]
+
+	]
+];
+
+$jsonCanceledSlipFlex = [
+	"type" => "flex",
+	"altText" => "Hello Flex Message",
+	"contents" => [
+	  "type" => "bubble",
+	  "direction" => "ltr",
+	  "header" => [
+		"type" => "box",
+		"layout" => "vertical",
+		"contents" => [
+		  [
+			"type" => "text",
+			"text" => "#55555",
+			"size" => "3xl",
+			"weight" => "bold",
+			"color" => "#000000"
+		  ],
+		  [
+			"type" => "text",
+			"text" => "นวดแผนไทย สุดผ่อนคลาย",
+			"size" => "lg",
+			"weight" => "bold",
+			"color" => "#000000"
+		  ],
+		  [
+			"type" => "text",
+			"text" => "2019.02.14 21:47 (GMT+0700)",
+			"size" => "xs",
+			"color" => "#B2B2B2"
+		  ]
+		]
+	  ],
+	  "body" => [
+		"type" => "box",
+		"layout" => "vertical",
+		"contents" => [
+		  [
+			"type" => "separator",
+			"color" => "#C3C3C3"
+		  ],
+		  [
+			"type" => "box",
+			"layout" => "baseline",
+			"margin" => "lg",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "x2",
+				"align" => "start",
+				"color" => "#C3C3C3"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "฿ 2000",
+				"align" => "end",
+				"color" => "#000000"
+			  ]
+			]
+		  ],
+		  [
+			"type" => "separator",
+			"margin" => "lg",
+			"color" => "#C3C3C3"
+		  ],
+		  [
+			"type" => "box",
+			"layout" => "baseline",
+			"margin" => "lg",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "ผู้จอง:",
+				"align" => "start",
+				"color" => "#C3C3C3"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "พี่รินริน",
+				"align" => "end",
+				"color" => "#000000"
+			  ]
+			]
+		  ],
+		  [
+			"type" => "box",
+			"layout" => "baseline",
+			"margin" => "lg",
+			"contents" => [
+			  [
+				"type" => "text",
+				"text" => "หมายเหตุ:",
+				"align" => "start",
+				"color" => "#C3C3C3"
+			  ],
+			  [
+				"type" => "text",
+				"text" => "ขอมือหนักๆ",
+				"align" => "end",
+				"color" => "#000000"
+			  ]
+			]
+		  ]
+		]
 	  ]
 	]
 ];
@@ -407,6 +533,7 @@ $jsonPromoFlex = [
 	  ]
 	]
 ];
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -432,6 +559,9 @@ if (!is_null($events['events'])) {
 		}
 		if ($event['type'] == 'message' && $event['message']['text'] == 'mybooking') {
 			$messages = $jsonSlipFlex;
+		}
+		if ($event['type'] == 'message' && $event['message']['text'] == 'ยกเลิกการจอง') {
+			$messages = $jsonCanceledSlipFlex;
 		}
 		if ($event['type'] == 'message' && $event['message']['text'] == 'promotion') {
 			$messages = $jsonPromoFlex;
