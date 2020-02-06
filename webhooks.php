6eeb05fdfa885b25ea89360c24919cca7e2f55b0
Tanee Yooyen <tiwly23060@gmail.com>
2020-02-06 15:46:32 +0700

    try json decode

--- webhooks.php
+++ webhooks.php
@@ -240,183 +240,186 @@ $jsonCanceledSlipFlex = [
 	]
 ];
 
-$jsonFlex = '{
-	"type": "carousel",
-	"contents": [
-	  {
-		"type": "bubble",
-		"hero": {
-		  "type": "image",
-		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover",
-		  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png"
-		},
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "text",
-			  "text": "Arm Chair, White",
-			  "wrap": true,
-			  "weight": "bold",
-			  "size": "xl"
-			},
-			{
-			  "type": "box",
-			  "layout": "baseline",
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "$49",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "xl",
-				  "flex": 0
-				},
-				{
-				  "type": "text",
-				  "text": ".99",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "sm",
-				  "flex": 0
-				}
-			  ]
-			}
-		  ]
-		},
-		"footer": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "style": "primary",
-			  "action": {
-				"type": "uri",
-				"label": "Add to Cart",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "button",
-			  "action": {
-				"type": "uri",
-				"label": "Add to wishlist",
-				"uri": "https://linecorp.com"
-			  }
-			}
-		  ]
-		}
-	  },
-	  {
-		"type": "bubble",
-		"hero": {
-		  "type": "image",
-		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover",
-		  "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
-		},
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "text",
-			  "text": "Metal Desk Lamp",
-			  "wrap": true,
-			  "weight": "bold",
-			  "size": "xl"
-			},
-			{
-			  "type": "box",
-			  "layout": "baseline",
-			  "flex": 1,
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "$11",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "xl",
-				  "flex": 0
-				},
-				{
-				  "type": "text",
-				  "text": ".99",
-				  "wrap": true,
-				  "weight": "bold",
-				  "size": "sm",
-				  "flex": 0
-				}
-			  ]
-			},
-			{
-			  "type": "text",
-			  "text": "Temporarily out of stock",
-			  "wrap": true,
-			  "size": "xxs",
-			  "margin": "md",
-			  "color": "#ff5551",
-			  "flex": 0
-			}
-		  ]
-		},
-		"footer": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "button",
-			  "flex": 2,
-			  "style": "primary",
-			  "color": "#aaaaaa",
-			  "action": {
-				"type": "uri",
-				"label": "Add to Cart",
-				"uri": "https://linecorp.com"
-			  }
-			},
-			{
-			  "type": "button",
-			  "action": {
-				"type": "uri",
-				"label": "Add to wish list",
-				"uri": "https://linecorp.com"
-			  }
-			}
-		  ]
-		}
-	  },
-	  {
-		"type": "bubble",
-		"body": {
-		  "type": "box",
-		  "layout": "vertical",
-		  "spacing": "sm",
-		  "contents": [
-			{
-			  "type": "text",
-			  "flex": 1,
-			  "gravity": "center",
-			  "action": {
-				"type": "uri",
-				"label": "See more",
-				"uri": "https://linecorp.com"
-			  }
-			}
-		  ]
-		}
-	  }
-	]
-  }';
+$jsonFlex = array (
+	'type' => 'bubble',
+	'hero' => 
+	array (
+	  'type' => 'image',
+	  'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+	  'size' => 'full',
+	  'aspectRatio' => '20:13',
+	  'aspectMode' => 'cover',
+	  'action' => 
+	  array (
+		'type' => 'uri',
+		'uri' => 'http://linecorp.com/',
+	  ),
+	),
+	'body' => 
+	array (
+	  'type' => 'box',
+	  'layout' => 'vertical',
+	  'contents' => 
+	  array (
+		0 => 
+		array (
+		  'type' => 'text',
+		  'text' => 'Brown Cafe',
+		  'weight' => 'bold',
+		  'size' => 'xl',
+		),
+		1 => 
+		array (
+		  'type' => 'box',
+		  'layout' => 'baseline',
+		  'margin' => 'md',
+		  'contents' => 
+		  array (
+			0 => 
+			array (
+			  'type' => 'icon',
+			  'size' => 'sm',
+			  'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+			),
+			1 => 
+			array (
+			  'type' => 'icon',
+			  'size' => 'sm',
+			  'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+			),
+			2 => 
+			array (
+			  'type' => 'icon',
+			  'size' => 'sm',
+			  'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+			),
+			3 => 
+			array (
+			  'type' => 'icon',
+			  'size' => 'sm',
+			  'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png',
+			),
+			4 => 
+			array (
+			  'type' => 'icon',
+			  'size' => 'sm',
+			  'url' => 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png',
+			),
+			5 => 
+			array (
+			  'type' => 'text',
+			  'text' => '4.0',
+			  'size' => 'sm',
+			  'color' => '#999999',
+			  'margin' => 'md',
+			  'flex' => 0,
+			),
+		  ),
+		),
+		2 => 
+		array (
+		  'type' => 'box',
+		  'layout' => 'vertical',
+		  'margin' => 'lg',
+		  'spacing' => 'sm',
+		  'contents' => 
+		  array (
+			0 => 
+			array (
+			  'type' => 'box',
+			  'layout' => 'baseline',
+			  'spacing' => 'sm',
+			  'contents' => 
+			  array (
+				0 => 
+				array (
+				  'type' => 'text',
+				  'text' => 'Place',
+				  'color' => '#aaaaaa',
+				  'size' => 'sm',
+				  'flex' => 1,
+				),
+				1 => 
+				array (
+				  'type' => 'text',
+				  'text' => 'Miraina Tower, 4-1-6 Shinjuku, Tokyo',
+				  'wrap' => true,
+				  'color' => '#666666',
+				  'size' => 'sm',
+				  'flex' => 5,
+				),
+			  ),
+			),
+			1 => 
+			array (
+			  'type' => 'box',
+			  'layout' => 'baseline',
+			  'spacing' => 'sm',
+			  'contents' => 
+			  array (
+				0 => 
+				array (
+				  'type' => 'text',
+				  'text' => 'Time',
+				  'color' => '#aaaaaa',
+				  'size' => 'sm',
+				  'flex' => 1,
+				),
+				1 => 
+				array (
+				  'type' => 'text',
+				  'text' => '10:00 - 23:00',
+				  'wrap' => true,
+				  'color' => '#666666',
+				  'size' => 'sm',
+				  'flex' => 5,
+				),
+			  ),
+			),
+		  ),
+		),
+	  ),
+	),
+	'footer' => 
+	array (
+	  'type' => 'box',
+	  'layout' => 'vertical',
+	  'spacing' => 'sm',
+	  'contents' => 
+	  array (
+		0 => 
+		array (
+		  'type' => 'button',
+		  'style' => 'link',
+		  'height' => 'sm',
+		  'action' => 
+		  array (
+			'type' => 'uri',
+			'label' => 'CALL',
+			'uri' => 'https://linecorp.com',
+		  ),
+		),
+		1 => 
+		array (
+		  'type' => 'button',
+		  'style' => 'link',
+		  'height' => 'sm',
+		  'action' => 
+		  array (
+			'type' => 'uri',
+			'label' => 'WEBSITE',
+			'uri' => 'https://linecorp.com',
+		  ),
+		),
+		2 => 
+		array (
+		  'type' => 'spacer',
+		  'size' => 'sm',
+		),
+	  ),
+	  'flex' => 0,
+	),
+  );
 $jsonPromoFlex = [
 	"type" => "flex",
 	"altText" => "Hello Flex Message",
