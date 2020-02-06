ba83ae71afd8fbfabac2ad80e7f1fc005346086c
Tanee Yooyen <tiwly23060@gmail.com>
2020-02-06 18:04:39 +0700

    rm hero

--- webhooks.php
+++ webhooks.php
@@ -381,13 +381,6 @@ $jsonShopFlex = [
 	"contents" => [
 	  "type" => "bubble",
 	  "direction" => "ltr",
-	  "hero" => [
-		"type" => "image",
-		"url" => "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_3_movie.png",
-		"size" => "full",
-		"aspectRatio" => "20:13",
-		"aspectMode" => "cover",
-	  ],
 	  "body" => [
 		"type" => "box",
 		"layout" => "vertical",
