e78389ff5124b4e24fb588c81e0c508da4ce03bc
Tew <tanee_yooyen@cmu.ac.th>
2020-03-05 18:12:25 +0700

    ok

--- webhooks.php
+++ webhooks.php
@@ -75,10 +75,7 @@ if (!is_null($events['events'])) {
 
 		}
 		if ($event['type'] == 'message' && $event['message']['text'] == 'services') {
-			$messages = [
-				'type' => 'flex',
-				'text' => $eiei
-			];
+			$messages = $eiei;
 		}
 
 		// Make a POST Request to Messaging API to reply to sender
@@ -88,7 +85,7 @@ if (!is_null($events['events'])) {
 		$url = 'https://api.line.me/v2/bot/message/reply';
 		$data = [
 			'replyToken' => $replyToken,
-			'messages' => [$messages],
+			'messages' => $messages,
 		];
 		$post = json_encode($data);
 		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
