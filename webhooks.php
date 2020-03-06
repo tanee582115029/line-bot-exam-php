a0bd9005a46a7494638e08690cce5a2fe8394afe
Tew <tanee_yooyen@cmu.ac.th>
2020-03-06 11:55:32 +0700

    ok

--- webhooks.php
+++ webhooks.php
@@ -29,18 +29,29 @@ if (!is_null($events['events'])) {
 
 		}
 		if ($event['type'] == 'message' && $event['message']['text'] == 'services') {
-			$jservice = json_decode(file_get_contents("services.json"),true);
-			$jservice['contents']['hero']['url'] = 'https://lh3.googleusercontent.com/proxy/-1c7kpqSBd9MstpLoL1SssBqYrwCIFRJEO0GHycFDq7ZfioJKWjNkF1Qn7jex6Z18Kr51k_W2Oa5vHimHAIDDMVdGQbvjHHu6tKcXSWQxfuOT8NSvjOfZGTi1VR8e9aYNA_y';
-			$jservice['contents']['body']['contents'][0]['text'] = 'นวดฝ่าเท้า';
-			$jservice['contents']['body']['contents'][1]['text'] = 'จ อ พฤ ศ';
-			$jservice['contents']['body']['contents'][2]['text'] = '9,999 ฿';
-			$messages = $jservice;
+			$service_json = json_decode(file_get_contents("services.json"),true);
+			$service_json['contents']['hero']['url'] = 'https://lh3.googleusercontent.com/proxy/-1c7kpqSBd9MstpLoL1SssBqYrwCIFRJEO0GHycFDq7ZfioJKWjNkF1Qn7jex6Z18Kr51k_W2Oa5vHimHAIDDMVdGQbvjHHu6tKcXSWQxfuOT8NSvjOfZGTi1VR8e9aYNA_y';
+			$service_json['contents']['body']['contents'][0]['text'] = 'นวดฝ่าเท้า';
+			$service_json['contents']['body']['contents'][1]['text'] = 'จ อ พฤ ศ';
+			$service_json['contents']['body']['contents'][2]['text'] = '9,999 ฿';
+			$messages = $service_json;
 		}
 		if ($event['type'] == 'message' && $event['message']['text'] == 'promotions') {
 			$messages = json_decode(file_get_contents("promotions.json"),true);
 		}
 		if ($event['type'] == 'message' && $event['message']['text'] == 'mybooking') {
-			$messages = json_decode(file_get_contents("mybooking.json"),true);
+			$code = '#46547';
+			$mybooking_json = json_decode(file_get_contents("mybooking.json"),true);
+			$mybooking_json['contents']['header']['contents'][0]['text'] = $code;
+			$mybooking_json['contents']['header']['contents'][1]['text'] = 'นวดฝ่าเท้า';
+			$mybooking_json['contents']['header']['contents'][2]['text'] = '12/12/2020, 19:00';
+			$mybooking_json['contents']['body']['contents'][1]['contents'][0]['text'] = 'x76';
+			$mybooking_json['contents']['body']['contents'][1]['contents'][1]['text'] = '9,899 ฿';
+			$mybooking_json['contents']['header']['contents'][3]['contents'][1]['text'] = 'เจ้น้ำ';
+			$mybooking_json['contents']['header']['contents'][4]['contents'][1]['text'] = 'ขอมือเบาๆ';
+			$mybooking_json['contents']['footer']['contents'][1]['text'] = 'cancel'.$code;
+
+			$messages = $mybooking_json;
 		}
 		if ($event['type'] == 'message' && $event['message']['text'] == 'cancel') {
 			$messages = json_decode(file_get_contents("cancel.json"),true);
